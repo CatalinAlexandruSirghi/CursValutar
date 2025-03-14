cd30ec2d12f596c39a374d2e620e0d49ef6e2070
CatalinAlexandruSirghi <catalinalexandrusirghi@gmail.com>
2025-03-14 20:36:20 +0200

    Made skeleton of ConvertorPage

--- Views/ConvertorPage.xaml.cs
+++ Views/ConvertorPage.xaml.cs
@@ -2,8 +2,22 @@ namespace CursValutar.Views;
 
 public partial class ConvertorPage : ContentPage
 {
-	public ConvertorPage()
-	{
-		InitializeComponent();
-	}
+    List<string> listaValute = [ "EUR", "RON"];
+    public ConvertorPage()
+    {
+        InitializeComponent();
+        PickerDestinatie.ItemsSource = listaValute;
+        PickerSursa.ItemsSource = listaValute;
+        
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        //TODO preluare valuta sursa (EUR -> 4.97 RON)
+        //Preluare valuta destinatie
+        //Preluare suma de convertit
+        //Efectuare conversie
+        //Afisare rezultat
+
+    }
 }
