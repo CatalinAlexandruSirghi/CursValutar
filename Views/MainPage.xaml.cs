c30f099778362c7bc0ff8f2560528715ca66916c
CatalinAlexandru <catalinalexandrusirghi@gmail.com>
2025-03-31 20:28:18 +0300

    Added support for flags

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -1,12 +1,17 @@
-﻿namespace CursValutar.Views
+﻿using CursValutar.Models;
+using CursValutar.Services;
+
+namespace CursValutar.Views
 {
     public partial class MainPage : ContentPage
     {
-
+        private List<Curs> listaValute = new List<Curs>();
 
         public MainPage()
         {
             InitializeComponent();
+            listaValute = CursBNRService.ObtineCurs("https://bnr.ro/nbrfxrates.xml");
+            ListViewCurs.ItemsSource = listaValute;
         }
 
         private void ToolbarItem_Clicked(object sender, EventArgs e)
